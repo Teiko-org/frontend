3f5916446b2f72db66c4c7f664a75e7d94753937
PajorVi <vinicius.marques@sptech.school>
2025-03-23 16:13:17 -0300

    add cadastro/login com json server

--- javascript-Utils-Login-Cadastro/scripts.js
+++ javascript-Utils-Login-Cadastro/scripts.js
@@ -1,8 +1,10 @@
+const baseUrl = "http://localhost:3000/users";
+
 document.addEventListener("DOMContentLoaded", () => {
     const registerContainer = document.getElementById('register-container');
     const loginContainer = document.getElementById('login-container');
     const site = document.getElementById('site');
-    
+
     document.body.addEventListener('click', handleButtonClick);
 
     document.getElementById("register-phone").addEventListener("input", formatPhoneNumber);
@@ -25,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 togglePassword(e.target);
                 break;
             case 'register':
-                registerUser();
+                verifyRegisterUser();
                 break;
             case 'login':
                 loginUser();
@@ -43,12 +45,12 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function formatPhoneNumber(e) {
-    let value = e.target.value.replace(/\D/g, ""); 
+    let value = e.target.value.replace(/\D/g, "");
     if (!value.startsWith("55")) {
         value = "55" + value;
     }
     value = value.slice(0, 13);
-    e.target.value = value.length >= 4 
+    e.target.value = value.length >= 4
         ? `+55 (${value.slice(2, 4)}) ${value.slice(4, 9)}-${value.slice(9)}`
         : `+55 (${value.slice(2)}`;
 }
@@ -69,31 +71,45 @@ function togglePassword(eyeIcon) {
     passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password';
 }
 
-function registerUser() {
+async function verifyRegisterUser() {
     const name = document.getElementById("register-name").value.trim();
     const phone = document.getElementById("register-phone").value.trim();
     const password = document.getElementById("register-password").value.trim();
     const confirmPassword = document.getElementById("register-confirm-password").value.trim();
-    
+
     if (!validateRegistration(name, phone, password, confirmPassword)) return;
-    
-    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const users = await listUsers();
+
     if (users.some(user => user.phone === phone)) {
         alert("Telefone já cadastrado");
         return;
     }
-    
-    users.push({ name, phone, password });
-    localStorage.setItem("users", JSON.stringify(users));
-    
+
+    registerUser(name, phone, password);
+
     alert("Usuário registrado com sucesso");
     toggleForm();
+}
+
+function registerUser(name, phone, password) {
+    const novoUsuario = {name, phone, password}
+    fetch(baseUrl, {
+        method: "POST",
+        body: JSON.stringify(novoUsuario)
+    })
+        .then((res) => {
+            alert("Usuário cadastrado com sucesso!");
+        })
+        .catch((err) => {
+            alert("Falha ao cadastrar usuário!");
+        })
 }
 
 function validateRegistration(name, phone, password, confirmPassword) {
     const phoneRegex = /^\+55 \(\d{2}\) \d{4,5}-\d{4}$/;
     const passwordRegex = /^(?=.*[A-Z])(?=.*[\W_]).{6,}$/;
-    
+
     if (!name || !phone || !password || !confirmPassword) {
         alert("Preencha todos os campos");
         return false;
@@ -113,30 +129,39 @@ function validateRegistration(name, phone, password, confirmPassword) {
     return true;
 }
 
-function loginUser() {
+async function loginUser() {
     const phone = document.getElementById("login-name").value.trim();
     const password = document.getElementById("login-password").value.trim();
     const rememberMe = document.getElementById("remember-me").checked;
-    
+
     if (!phone || !password) {
         alert("Preencha todos os campos");
         return;
     }
-    
-    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const users = await listUsers();
     const user = users.find(user => user.phone === phone && user.password === password);
-    
+
     if (!user) {
         alert("Usuário ou senha inválidos");
         return;
     }
-    
+
     if (rememberMe) {
         localStorage.setItem("rememberedUser", phone);
     } else {
         localStorage.removeItem("rememberedUser");
     }
-    
+
     alert(`Bem-vindo, ${user.name}`);
     closeContainers();
+}
+
+async function listUsers() {
+    try {
+        const res = await fetch(baseUrl);
+        return await res.json();
+    } catch (e) {
+        alert("Falha ao listar usuários");
+    }
 }
