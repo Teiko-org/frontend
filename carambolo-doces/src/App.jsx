1952954870401d2704e084fb33212d4eb84f9bfc
GustavoAloe <gustavo.moraes@sptech.school>
2025-04-18 00:00:06 -0300

    feat: componentes básicos, login e cadastro

---
+++ carambolo-doces/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ButtonArrow from "./components/ButtonArrow";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
