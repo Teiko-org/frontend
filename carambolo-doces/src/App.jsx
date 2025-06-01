4b40aea3843e4d544cc5fe2762f6d96914cd0d34
SamaraLisboa <samara.bueno@sptech.school>
2025-06-01 17:05:36 -0300

    modal para cadastro de produtos

--- carambolo-doces/src/App.jsx
+++ carambolo-doces/src/App.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import "./App.css";
 import AppRoutes from "./routes/routes";
 import BarraLateralDashboard from "./components/BarraLateralDashboard";
+import ModalCadastroProduto from "./components/ModalCadastroProduto";
+import ModalBase from "./components/ModalBase";
+import ModalBaseForm from "./components/ModalBaseForm";
 
 function App() {
-  return <BarraLateralDashboard/>
+  return <ModalCadastroProduto/>
 }
 
 export default App;
--- carambolo-doces/src/components/Button/index.jsx
+++ carambolo-doces/src/components/Button/index.jsx
@@ -2,19 +2,23 @@ import React from "react";
 
 function Button({ 
   text, 
+  children,
   onClick,
   bgColor = "bg-gradient-to-l from-gold to-darkGold", 
   fontSize = "text-lg", 
   textColor = "text-blue", 
   borderColor = "border-gold",
-  className = ""
+  className = "",
+  type = "button"
 }) {
   return (
     <button
+    type={type}
       onClick={onClick}
       className={`${bgColor} ${fontSize} ${textColor} ${borderColor} font-bold py-1 px-4 rounded-full shadow-md border-2 focus:outline-none transform hover:scale-105 transition-transform ${className}`}
     >
       {text}
+      {children}
     </button>
   );
 }
