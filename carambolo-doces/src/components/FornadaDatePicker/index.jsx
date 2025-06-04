a47e7a1e565305e8ad067cc9382cd777575248e7
Raine-Jardim <raine.jardim@sptech.school>
2025-06-04 15:13:47 -0300

    Componentes página Dashboard de Fornadas

---
+++ carambolo-doces/src/components/FornadaDatePicker/index.jsx
@@ -0,0 +1,22 @@
+import DatePicker from "../DatePicker";
+
+function FornadaDatePicker() {
+
+    return (
+        <div
+            className={`flex flex-col justify-center items-center w-[470px] h-[170px]  border-2 border-gold rounded-2xl bg-bgNativeHome gap-5 p-10`}
+        >
+            <h3 className="font-bold">Iniciar Nova Fornada</h3>
+
+            <div className="flex justify-center gap-20">
+                    <DatePicker label={"De:"}/>
+
+                    <DatePicker label={"Até:"}/>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default FornadaDatePicker;
