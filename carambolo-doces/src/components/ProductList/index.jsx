26ca3bba545336e10c0639a73282db1f704bd04f
PajorVi <vinicius.marques@sptech.school>
2025-06-08 20:50:47 -0300

    finaliza estilização tela produtos

--- carambolo-doces/src/components/ProductList/index.jsx
+++ carambolo-doces/src/components/ProductList/index.jsx
@@ -15,6 +15,7 @@ import Button from '../Button';
 import { CiFilter } from "react-icons/ci";
 import ModalFilterProduct from '../ModalFilterProduct';
 import { findAllBolo, findAllFornada, handleDeleteBolo, handleVisibilityBolo, handleVisibilityProdutoFornada } from '../../service/productService';
+import { LuSearch } from "react-icons/lu";
 
 const columns = [
     { id: 'ativo', label: '', minWidth: 50, align: 'left' },
@@ -120,15 +121,16 @@ export default function ProductList() {
             <div className='flex flex-row justify-between items-center bg-gradient-blue h-[4.6875rem] w-full'>
                 <h1 className='bg-gradient-gold text-transparent bg-clip-text pl-[5%] text-[1.5rem] font-bold'>Listagem de produtos</h1>
                 <div className='pr-[5%] w-[45%] flex flex-row justify-between'>
-                    <div className='h-[100%] w-[60%]'>
+                    <div className='h-[100%] w-[60%] flex relative'>
                         <input
                             type="text"
                             placeholder='Procurar por produto'
                             className='h-[38px] w-[100%] pl-2 rounded-lg'
                             onChange={(e) => setSearchTerm(e.target.value)}
                         />
+                    <LuSearch className='absolute left-[90%] top-1.5 text-[1.625rem] text-[#A47032]'/>
                     </div>
-                    <Button text={'FILTRAR'} children={<CiFilter />} onClick={() => setFilterModalOpen(true)} className='flex flex-row items-center' />
+                    <Button text={'FILTRAR'} children={<CiFilter className='text-[1.625rem]'/>} onClick={() => setFilterModalOpen(true)} className='flex flex-row items-center' />
                     {
                         isFilterModalOpen && (
                             <ModalFilterProduct products={products} setFilterModalOpen={setFilterModalOpen} onClose={() => setFilterModalOpen(false)} />
@@ -138,7 +140,7 @@ export default function ProductList() {
             </div>
 
             <Paper sx={{ width: '100%', maxHeight: '100%', overflow: 'hidden', border: 'none', boxShadow: 'none' }} >
-                <TableContainer sx={{ maxHeight: 440 }} className='bg-bgNativeHome'>
+                <TableContainer sx={{ maxHeight: 440 }} className='bg-[#FFE7DD]'>
                     <Table stickyHeader aria-label="sticky table">
                         <TableHead>
                             <TableRow>
@@ -146,8 +148,8 @@ export default function ProductList() {
                                     <TableCell
                                         key={column.id}
                                         align={column.align}
-                                        style={{ minWidth: column.minWidth }}
-                                        sx={{ backgroundColor: "transparent", fontWeight: "bold", boxShadow: "none", borderBottom: "none", paddingTop: "0.5rem", paddingBottom: "0.5rem" }}
+                                        style={{ minWidth: column.minWidth, height: '50px' }}
+                                        sx={{ backgroundColor: "transparent", fontWeight: "bold", boxShadow: "none", borderBottom: "none", paddingTop: "0.5rem", paddingBottom: "0.5rem", padding: 0}}
                                     >
                                         {column.label}
                                     </TableCell>
@@ -161,32 +163,60 @@ export default function ProductList() {
                                     role="checkbox"
                                     tabIndex={-1}
                                     key={`${row.id}-${row.categoria}`}
-                                    className={`${index % 2 === 0 ? 'bg-[#FFEEE7]' : 'bg-[#FFE7DD]'}`}
+                                    className={`${index % 2 === 0 ? 'bg-[#FFE7DD]' : ' bg-[#FFEEE7]'} h-10`}
+                                    sx={{ boxShadow: "none", borderBottom: "none" }}
                                 >
                                     {columns.map((column) => {
                                         const value = row[column.id];
                                         if (column.id === 'ativo') {
                                             return (
-                                                <TableCell key={column.id} align={column.align} className='rounded-l-full'>
+                                                <TableCell key={column.id} align={column.align} className='rounded-l-full' sx={{ boxShadow: "none", borderBottom: "none", padding: 0, paddingLeft: '1.25rem' }}>
                                                     {row.isAtivo
-                                                        ? <LuEye className='w-5 cursor-pointer' onClick={() => handleVisibility(row.id, row.categoria)} />
-                                                        : <LuEyeClosed className='w-5 cursor-pointer' onClick={() => handleVisibility(row.id, row.categoria)} />}
+                                                        ? <LuEye className='w-5 cursor-pointer text-[#A47032] text-[1.625rem]' onClick={() => handleVisibility(row.id, row.categoria)} />
+                                                        : <LuEyeClosed className='w-5 cursor-pointer text-[#A47032] text-[1.625rem]' onClick={() => handleVisibility(row.id, row.categoria)} />}
                                                 </TableCell>
                                             );
                                         }
 
+                                        if (column.id === 'produto') {
+                                            return (
+                                                <TableCell key={column.id} align={column.align} sx={{ boxShadow: "none", borderBottom: "none", padding: 0 }}>
+                                                    {row.produto}
+                                                </TableCell>
+                                            )
+                                        }
+
+                                        if (column.id === 'categoria') {
+                                            return (
+                                                <TableCell key={column.id} align={column.align} sx={{ boxShadow: "none", borderBottom: "none", padding: 0 }}>
+                                                    {row.categoria}
+                                                </TableCell>
+                                            )
+                                        }
+
+                                        if (column.id == 'preco') {
+                                            return (
+                                                <TableCell key={column.id} align={column.align} sx={{ boxShadow: "none", borderBottom: "none", padding: 0 }}>
+                                                    <span>{"R$: " + row.valor.toFixed(2)}</span>
+                                                </TableCell>
+                                            )
+                                        }
+
                                         if (column.id === 'quantidade') {
                                             if (row.categoria.includes("carambolo")) {
-                                                return <TableCell key={column.id} align={column.align}>-</TableCell>;
+                                                return <TableCell key={column.id} align={column.align} sx={{ boxShadow: "none", borderBottom: "none", padding: 0 }}>-</TableCell>;
+                                            } else {
+                                                return <TableCell key={column.id} align={column.align} sx={{ boxShadow: "none", borderBottom: "none", padding: 0 }}>{row.quantidade}</TableCell>
                                             }
                                         }
 
                                         if (column.id === 'status') {
                                             if (row.categoria.includes("carambolo")) {
-                                                return <TableCell key={column.id} align={column.align}>-</TableCell>;
+                                                return <TableCell key={column.id} align={column.align} sx={{ boxShadow: "none", borderBottom: "none", padding: 0 }}>-</TableCell>;
                                             }
+
                                             return (
-                                                <TableCell key={column.id} align={column.align}>
+                                                <TableCell key={column.id} align={column.align} sx={{ boxShadow: "none", borderBottom: "none", padding: 0 }}>
                                                     <div className='flex flex-row justify-center items-center gap-2'>
                                                         <div className={`rounded-full min-w-2 min-h-2 ${row.quantidade <= 0 ? 'bg-[#D70000]' : 'bg-[#00AF2F]'}`} />
                                                         <span>{row.quantidade <= 0 ? 'Indisponível' : 'Disponível'}</span>
@@ -197,9 +227,9 @@ export default function ProductList() {
 
                                         if (column.id === 'edit') {
                                             return (
-                                                <TableCell key={column.id} align={column.align} className='rounded-e-full items-center'>
+                                                <TableCell key={column.id} align={column.align} className='rounded-e-full items-center' sx={{ boxShadow: "none", borderBottom: "none", padding: 0, paddingRight: '1.25rem' }}>
                                                     <div className='flex justify-end'>
-                                                        <FaRegEdit />
+                                                        <FaRegEdit className='text-[#A47032] text-[1.625rem]' />
                                                     </div>
                                                 </TableCell>
                                             );
@@ -223,21 +253,6 @@ export default function ProductList() {
                                         //     );
                                         // }
 
-                                        if (column.id == 'preco') {
-                                            return (
-                                                <TableCell key={column.id} align={column.align}>
-                                                    <span>{row.valor}</span>
-                                                </TableCell>
-                                            )
-                                        }
-
-                                        return (
-                                            <TableCell key={column.id} align={column.align}>
-                                                {column.format && typeof value === 'number'
-                                                    ? column.format(value)
-                                                    : value}
-                                            </TableCell>
-                                        );
                                     })}
 
                                 </TableRow>
