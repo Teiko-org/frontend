6d269c21dce8dd48ea2324cde6e9c2e14637ae1b
MuriloNasciment0 <murilo.barros@sptech.school>
2025-06-08 21:37:16 -0300

    Add routing and toast notifications to the App component; update routes to include FornadaOrderPage

--- carambolo-doces/src/App.jsx
+++ carambolo-doces/src/App.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import "./App.css";
 import AppRoutes from "./routes/routes";
-
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  
-  return <AppRoutes />
-  
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        style={{ zIndex: 9999 }}
+      />
+    </BrowserRouter>
+  );
 }
 
 export default App;
