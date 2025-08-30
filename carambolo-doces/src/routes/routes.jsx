ca3d8ab4e05cfd9412c11de34cbd60a2a252b159
PajorVi <vinicius.marques@sptech.school>
2025-08-30 17:30:25 -0300

    add secure routes

--- carambolo-doces/src/routes/routes.jsx
+++ carambolo-doces/src/routes/routes.jsx
@@ -10,20 +10,56 @@ import FornadaOrderPage from '../pages/FornadaOrderPage';
 import FornadaDashboard from '../pages/FornadaDashboard';
 import Products from '../pages/Products/index';
 import OrderKanban from '../pages/OrderKanban/index';
+import ProtectedRoute from './ProtectedRoute';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/pagina-usuario" element={<UserPage />} />
-      <Route path="/pagina-enderecos" element={<AddressPage />} />
+      <Route
+        path="/pagina-usuario"
+        element={
+          <ProtectedRoute>
+            <UserPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/pagina-enderecos"
+        element={
+          <ProtectedRoute>
+            <AddressPage />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/pedido-bolo" element={<Application />} />
-      <Route path="/carambolos" element={<Carambolos/>} />
-      <Route path="/fornada" element={<FornadaSemana/>} />
-      <Route path="/pedido-fornada" element={<FornadaOrderPage/>} />
-      <Route path="/dashboard-kanban-pedidos" element={<OrderKanban />} />
-      <Route path="/produtos" element={<Products />} />
-      <Route path="/fornada-dashboard" element={<FornadaDashboard/>} />
+      <Route path="/carambolos" element={<Carambolos />} />
+      <Route path="/fornada" element={<FornadaSemana />} />
+      <Route path="/pedido-fornada" element={<FornadaOrderPage />} />
+      <Route
+        path="/dashboard-kanban-pedidos"
+        element={
+          <ProtectedRoute requireAdmin={true}>
+            <OrderKanban />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/produtos"
+        element={
+          <ProtectedRoute requireAdmin={true}>
+            <Products />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/fornada-dashboard"
+        element={
+          <ProtectedRoute requireAdmin={true}>
+            <FornadaDashboard />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 };
