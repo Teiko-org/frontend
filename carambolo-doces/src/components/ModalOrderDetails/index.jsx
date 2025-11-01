164a1d82a5434d6ca49e103e611f365f93eed4c0
MuriloNasciment0 <murilo.barros@sptech.school>
2025-11-01 19:07:00 -0300

    Refactor import statement for OrderStatusChanger component and add informative message in Step1 component regarding automatic field filling. This enhances user guidance during the cake order process.

--- carambolo-doces/src/components/ModalOrderDetails/index.jsx
+++ carambolo-doces/src/components/ModalOrderDetails/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import OrderStatusChanger from "../orderStatusChanger";
+import OrderStatusChanger from "../OrderStatusChanger";
 
 function formatPhone(phone) {
   if (!phone) return "Carregando...";
